53c538576b54ec91640d22d62f38b0436e722bb0
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-12-22 14:03:03 +0545

    refactor: Overview: Small refactor for tailwind class in overview cards

--- components/overview-cards.tsx
+++ components/overview-cards.tsx
@@ -197,7 +197,7 @@ export function OverviewCards() {
 
           {/* Shine effect on hover */}
           <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-700">
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/10 to-transparent -translate-x-full group-hover:translate-x-full transition-transform duration-1000" />
+            <div className="absolute inset-0 bg-linear-to-r from-transparent via-white/10 to-transparent -translate-x-full group-hover:translate-x-full transition-transform duration-1000" />
           </div>
 
           {/* Content */}
